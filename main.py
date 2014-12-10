1c9595223a9a44c619d383d87878624696ab73d0
Brian Mansfield <bmansfieldhfl@gmail.com>
2014-12-09 20:52:16 -0500

    main.py
    Add constants needed throughout the game

---
+++ main.py
@@ -0,0 +1,20 @@
+# Defining the many constants used throughout the game
+
+##### CONSTANTS #####
+
+# Window
+FPS = 30
+WINWIDTH = 640
+WINHEIGHT = 480
+SNOWBACKGROUND = (255, 255, 255)  # white
+BLUE = (0, 0, 255)
+
+# Game Constants
+ENEMYMOVERATE = 9
+INVULNTIME = 2    # how long after being hit
+GAMEOVERTIME = 4  # how long the game over screen lasts
+HEALTHSTART = 3
+MAXENEMIES = 4
+STARTSCREENTIME = 3
+DAYLIGHT = 1
+NIGHT = 0
